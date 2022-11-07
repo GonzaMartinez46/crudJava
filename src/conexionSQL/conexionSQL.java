129a8e0fb25d82b4e91533f4babcde29ec101dce
GonzaMartinez46 <gonzamartinez46@gmail.com>
2022-11-07 10:52:37 +0500

    primer commit

---
+++ src/conexionSQL/conexionSQL.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package conexionSQL;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author gonza
+ */
+public class conexionSQL {
+    
+    Connection conectar=null;
+    String url="jdbc:mysql://localhost:3307/escuela";
+    String user="root";
+    String password="";
+    
+    public Connection conexion(){
+        try{
+            
+            conectar= DriverManager.getConnection(url, user, password);
+            
+            JOptionPane.showMessageDialog(null, "Conexion Exitosa");
+        }catch (Exception e){
+            JOptionPane.showMessageDialog(null, "error conexion "+e.getMessage());
+        }
+        
+        return conectar;
+    }
+    
+    
+}
